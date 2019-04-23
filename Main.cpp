e01017963bd5193c459377582753aa3bc6fae044
Jon Fisher <jondavidfisher@gmail.com>
2019-04-23 11:33:54 -0700

    initial commit

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string numberStr;
+	
+	cout << "enter a number" << endl;
+	cin >> numberStr;
+	cout << "you entered" << numberStr << endl;
+
+
+
+return 0;
+}
